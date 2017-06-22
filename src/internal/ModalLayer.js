3ab5e2315b884487d87aad91fe2f4a930563818a
chrisHchen <chenchris1986@163.com>
2017-06-23 07:57:38 +0800

    add ModalLayer

---
+++ src/internal/ModalLayer.js
@@ -0,0 +1,93 @@
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+import {unstable_renderSubtreeIntoContainer, unmountComponentAtNode} from 'react-dom';
+import './ModalLayer.css';
+import Dom from '../utils/dom';
+
+class ModalLayer extends Component {
+  static propTypes = {
+    componentClickAway: PropTypes.func,
+    open: PropTypes.bool.isRequired,
+    render: PropTypes.func.isRequired,
+    useLayerForClickAway: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    useLayerForClickAway: true,
+  }
+
+  componentDidMount() {
+    this.renderLayer();
+  }
+
+  componentDidUpdate() {
+    this.renderLayer();
+  }
+
+  componentWillUnmount() {
+    this.unrenderLayer();
+  }
+
+  onClickAway = (event) => {
+    if (event.defaultPrevented ||
+        !this.props.componentClickAway ||
+        !this.props.open) {
+      return;
+    }
+
+    const el = this.layer;
+    if (event.target !== el && event.target === window ||
+      (document.documentElement.contains(event.target) && !Dom.isDescendant(el, event.target))) {
+      this.props.componentClickAway(event);
+    }
+  };
+
+  unrenderLayer() {
+    if (!this.layer) {
+      return;
+    }
+    if (this.props.useLayerForClickAway) {
+      this.layer.style.position = 'relative';
+      this.layer.removeEventListener('touchstart', this.onClickAway);
+      this.layer.removeEventListener('click', this.onClickAway);
+    } else {
+      window.removeEventListener('touchstart', this.onClickAway);
+      window.removeEventListener('click', this.onClickAway);
+    }
+
+    unmountComponentAtNode(this.layer);
+    document.body.removeChild(this.layer);
+    this.layer = null;
+  }
+
+  renderLayer() {
+    const {open, render} = this.props;
+    if (open) {
+      if (!this.layer) {
+        this.layer = document.createElement('div');
+        document.body.appendChild(this.layer);
+        this.layer.className = 'zui-modalLayer';
+        if (this.props.useLayerForClickAway) {
+          this.layer.addEventListener('touchstart', this.onClickAway);
+          this.layer.addEventListener('click', this.onClickAway);
+        } else {
+          setTimeout(() => {
+            window.addEventListener('touchstart', this.onClickAway);
+            window.addEventListener('click', this.onClickAway);
+          }, 0);
+        }
+
+        const layerElement = render();
+        this.layerElement = unstable_renderSubtreeIntoContainer(this, layerElement, this.layer);
+      }
+    } else {
+      this.unrenderLayer();
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default ModalLayer;
