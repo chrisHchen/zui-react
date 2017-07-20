3399bc78ab6003bfa7e397cb73037b3749c190d9
chrisHchen <chenchris1986@163.com>
2017-07-20 09:08:36 +0800

    better Dialog

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -6,10 +6,13 @@ import Shade from '../Shade';
 import keycode from 'keycode';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import ClassNames from 'classnames';
+import Button from '../Button';
+import Clear from '../svg-embedded/clear';
 import './Dialog.css';
 
 class Dialog extends Component {
   static propTypes = {
+    actions: PropTypes.node,
     autoDetectWindowHeight: PropTypes.bool,
     autoScrollBodyContent: PropTypes.bool,
     bodyClassName: PropTypes.string,
@@ -19,8 +22,16 @@ class Dialog extends Component {
     contentStyle: PropTypes.object,
     modal: PropTypes.bool,
     onRequestClose: PropTypes.func,
+    onRequestConfirm: PropTypes.func,
     open: PropTypes.bool.isRequired,
     repositionOnUpdate: PropTypes.bool,
+    showClose: PropTypes.bool,
+    size: PropTypes.oneOf([
+      'tiny',
+      'small',
+      'large',
+      'full',
+    ]),
     style: PropTypes.object,
     title: PropTypes.string,
   }
@@ -30,6 +41,9 @@ class Dialog extends Component {
     autoScrollBodyContent: false,
     modal: false,
     repositionOnUpdate: true,
+    size: 'small',
+    showClose: true,
+    title: '提示',
   };
 
   requestClose(buttonClicked) {
@@ -46,24 +60,78 @@ class Dialog extends Component {
     this.requestClose(false);
   };
 
+  handleClickCancel = () => {
+    this.requestClose(true);
+  }
+
+  handleClickConfirm = () => {
+    if (this.props.onRequestConfirm) {
+      this.props.onRequestConfirm();
+    }
+  }
+
   handleKeyUp = (event) => {
     if (keycode(event) === 'esc') {
       this.requestClose(false);
     }
   };
 
+  createFooterElement() {
+    const footer = (
+      <div className="zui-dialog-footer">
+        {
+          React.Children.count(this.props.actions) > 0 ?
+            React.Children.toArray(this.props.actions) :
+            [
+              <Button
+                type="text"
+                label="取消"
+                key="cancel"
+                onClick={this.handleClickCancel}
+              />,
+              <Button
+                label="确定"
+                key="confirm"
+                onClick={this.handleClickConfirm}
+              />,
+            ]
+        }
+      </div>
+    );
+
+    return footer;
+  }
+
   render() {
     const {
       children,
       title,
       open,
+      size,
+      showClose,
     } = this.props;
 
-    const titleElement = <div className="zui-dialog-title">{title}</div>;
+    const titleElement = (
+      <div className="zui-dialog-header">
+        <span className="zui-dialog-title">{title}</span>
+        { showClose &&
+          <Clear className="zui-dialog-close" onClick={this.handleClickCancel} />
+        }
+      </div>
+    );
+
+    const footerElement = this.createFooterElement();
+
     const rootClassName = ClassNames({
       'zui-dialog': true,
       'is-open': !!open,
     });
+
+    const bodyClassName = ClassNames({
+      'zui-dialog-body': true,
+      [`zui-dialog-${size}`]: true,
+    });
+
     return (
       <div className={rootClassName}>
         {open &&
@@ -82,11 +150,12 @@ class Dialog extends Component {
         >
           {
             open &&
-            <Shade zDepth={4} className="zui-dialog-body" key="shade">
+            <Shade zDepth={4} className={bodyClassName} key="shade">
               {titleElement}
               <div className="zui-dialog-content">
                 {children}
               </div>
+              {footerElement}
             </Shade>
           }
         </CSSTransitionGroup>
