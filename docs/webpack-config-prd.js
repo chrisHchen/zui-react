7875af9033e4eb000dc0d9a076064a218182af13
chrisHchen <chenchris1986@163.com>
2017-07-24 21:29:35 +0800

    add build for docs

---
+++ docs/webpack-config-prd.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
+const plugins = [
+  new CleanWebpackPlugin(path.join(__dirname, './build'), {
+    root: path.join(__dirname, './'),
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+  }),
+  new ExtractTextPlugin('index.css'),
+];
+
+module.exports = {
+  entry: path.join(__dirname, './src/app.js'),
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  output: {
+    path: path.resolve(__dirname, './static'),
+    filename: '[name].js',
+  },
+  plugins: plugins,
+  stats: {
+    color: true,
+    reason: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+          ]}),
+      },
+    ],
+  },
+};
